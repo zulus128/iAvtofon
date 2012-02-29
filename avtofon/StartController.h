5372009747256cbacdd5e83031e59bdf7810ca2f
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-29 07:11:21 +0100

    5

---
+++ avtofon/StartController.h
@@ -0,0 +1,13 @@
+//
+//  StartController.h
+//  avtofon
+//
+//  Created by Mac on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartController : UIViewController
+
+@end
